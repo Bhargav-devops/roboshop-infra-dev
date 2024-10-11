da00d3159a823c69e12fd19ec88a9febc432758d
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-10-11 10:00:18 +0530

    roboshop-infra-dev

--- 04-Database/bootstrap.sh
+++ 04-Database/bootstrap.sh
@@ -4,4 +4,4 @@ environment = $2
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible boto boto3
 
-ansible-pull -u https://github.com/Bhargav-devops/roroboshop-ansible-roles-tf.git -component=mongodb -environment=dev main-tf.yaml
+ansible-pull -u https://github.com/Bhargav-devops/roroboshop-ansible-roles-tf.git -e component=$component -e env=$environment main-tf.yaml
--- 04-Database/ec2.tf
+++ 04-Database/ec2.tf
@@ -16,17 +16,17 @@ module "mongodb" {
     )
 }
 
-resource "null_resource" "cluster" {
+resource "null_resource" "mongodb" {
   # Changes to any instance of the cluster requires re-provisioning
   triggers = {
-    cluster_instance_ids = module.mongodb.id
+    instance_id = module.mongodb.id
   }
 
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
   connection {
+    host = module.mongodb.private_ip
     type = "ssh"
-    host = module.mongodb.id
     user = "centos"
     password = "DevOps321"
   }
@@ -39,8 +39,55 @@ resource "null_resource" "cluster" {
   provisioner "remote-exec" {
     # Bootstrap script called with private_ip of each node in the cluster
     inline = [
-        "chmod +x /tmp/bootstrap.sh"
-        "sudo sh /tmp/bootstrap.sh"
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mongodb dev"
+    ]
+  }
+}
+
+module "redis" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  name = "${local.ec2_name}-redis"
+  ami = data.aws_ami.centos8.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.redis_sg_id.value]
+  subnet_id              = local.database_subnet_id
+    tags =  merge(
+        var.common_tags,
+        {
+            component = "redis"
+        },
+        {
+            Name = "${local.ec2_name}-redis"
+        }
+    )
+}
+
+resource "null_resource" "redis" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = module.redis.id
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host = module.redis.private_ip
+    type = "ssh"
+    user = "centos"
+    password = "DevOps321"
+  }
+
+    provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+    }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh redis dev"
     ]
   }
 }
